6ba819c53c4e24c3b107ecbc2f254197c30484f3
Lvl3Mage <lvlthreemage@gmail.com>
2021-02-11 21:52:30 +0100

    Added project files
    Added project files to the repository

---
+++ Ivengo-site/js/default.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+$('.ibg').each(function(index, el) {
+	str = '';
+	$(this).children('img').each(function(index, el) {
+		if (index>0){
+			str += ', ';
+		}
+		str += "url('" + $(el).prop('src') + "')";
+		$(el).remove();
+	});
+	$(this).css('background-image', str);
+});
+
+});
---
+++ Ivengo-site/source/js/default.js
@@ -0,0 +1,4 @@
+//@prepros-append begin.js
+//@prepros-append ibg.js
+//@prepros-append script.js
+//@prepros-append end.js
